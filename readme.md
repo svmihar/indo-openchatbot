6ad1247dffa55464b6fd584b2dcb30f29f051c9a
svmihar <svmihar@gmail.com>
2020-05-16 16:22:05 +0700

    :fire: new subtitle downloader

--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 ~~1. acquire dataset~~
-- bikin biar bisa download dan extract zip
+~~- bikin biar bisa download dan extract zip~~
+- preproses semua link agar ke subtitle yify aja
 
 2. preprocess dataset to an appropriate format for gpt2
--- subtitle.py
+++ subtitle.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import time
 from dataclasses import dataclass
 from selenium.webdriver import Chrome
 import pandas
@@ -40,15 +41,36 @@ class scraper:
     prefs["profile.managed_default_content_settings"] = {"images": 2}
     chrome_options.add_argument("--disable-popup-blocking")
     chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     driver = Chrome(executable_path='../../chromedriver',
                     options=chrome_options)
-    url: str
-    page_length: int
+    csv: str
+
+    def __post_init__(self):
+        self.contoh_df = pd.read_csv(self.csv)  # ['link'].values
+        self.hasil = [self.contoh_df['link'][i] for i, title in enumerate(self.contoh_df.titles)
+                      if 'yts' in title or 'yify' in title]
+
+    def set_url(self, link):
+        self.url = link
+        self.driver.get(self.url)
+        self.driver.find_element_by_xpath(
+            '//*[@id="subtitle-info"]/div/div[2]/a/i').click()
+
+    def run(self):
+        for link in tqdm(self.hasil):
+            try:
+                self.set_url(link)
+            except Exception as e:
+                print(e)
+                pass
+        time.sleep(5)
+        self.driver.quit()
 
 
 if __name__ == "__main__":
-    page_range = range(1, 1143)
-    for i in tqdm(page_range):
-        page = get_page(page=i)
-        page.to_csv(f'./data/{i}.csv', index=False)
+    c = scraper('./data/gabungan.csv')
+    c.run()
+    # for i in tqdm(page_range):
+    #     page = get_page(page=i)
+    #     page.to_csv(f'./data/{i}.csv', index=False)
